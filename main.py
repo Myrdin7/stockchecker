d2dd31d4dffaa6ff64278df82bd58d9e80e674b2
Myrdin van der Horst <m.vanderhorst@districon.com>
2021-02-27 20:30:48 +0100

    lol

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ with open('user_agents.txt') as f:
 
 shops = {'coolblue':{'urls':['https://www.coolblue.nl/product/865866/playstation-5.html', 
                             'https://www.coolblue.nl/product/869587/sony-playstation-5-dualsense-draadloze-controller.html']
-                            }}
+                            },
+        'amazonde':{'urls':['https://www.amazon.de/PS5-Konsole-Sony-PlayStation-Standard/dp/B08VLX84G6/',
+                            'https://www.amazon.de/Sony-DualSense-Wireless-Controller-PlayStation-5/dp/B08H93ZRK9/'],
+        'amazonnl':{'urls':['https://www.amazon.nl/Sony-PlayStation-PlayStation®5-Console/dp/B08H93ZRK9',
+                            'https://www.amazon.nl/Sony-PlayStation-9399506-PlayStation®5-Controller/dp/B08H97WTBL/'],
+        }
+}
 
 users = {'Myrdin':'u2peec5j2cihqg6jp2ozez5v78nr8p',
          'Sven':'us51jg8q6kw25hmugxgx8zp8r76nci'
@@ -32,6 +38,8 @@ def get_random_ua():
 def main_loop():
     while True:
         coolblue()
+        amazonde()
+        amazonnl()
         time.sleep(120)
 
 def coolblue():
@@ -47,6 +55,37 @@ def coolblue():
             send_notification('Coolblue', shops['coolblue']['urls'][0])
     except:
         print("Error for Coolblue, message: {} - {}".format(req.status_code, req.reason))
+
+
+def amazonde():
+    try:
+        req = requests.get(shops['amazonde']['urls'][0], headers={'User-Agent':get_random_ua()})
+        print("Coolblue - {} - {}".format(req.status_code, req.reason))
+        soup = BeautifulSoup(req.text, 'lxml')
+        in_cart = soup.find("input", {"id": "add-to-cart-button"})
+        if in_cart == None:
+            print("Out of stock")
+        elif in_cart != None:
+            print("In stock!!")
+            send_notification('Amazon DE', shops['amazonde']['urls'][0])
+    except:
+        print("Error for Amazon DE, message: {} - {}".format(req.status_code, req.reason))
+
+
+def amazonnl():
+    try:
+        req = requests.get(shops['amazonnl']['urls'][0], headers={'User-Agent':get_random_ua()})
+        print("Coolblue - {} - {}".format(req.status_code, req.reason))
+        soup = BeautifulSoup(req.text, 'lxml')
+        in_cart = soup.find("input", {"id": "add-to-cart-button"})
+        if in_cart == None:
+            print("Out of stock")
+        elif in_cart != None:
+            print("In stock!!")
+            send_notification('Amazon NL', shops['amazonnl']['urls'][0])
+    except:
+        print("Error for Amazon NL, message: {} - {}".format(req.status_code, req.reason))
+
 
 
 def send_notification(shop, url):
