e71e38d1b1e2f5c6df7780beafe5241140ec474f
Myrdin van der Horst <m.vanderhorst@districon.com>
2021-02-27 19:17:51 +0100

    joe

--- main.py
+++ main.py
@@ -1,17 +1,74 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+from pushover import init, Client
+import numpy as np
+
+from config import Config
+
+# headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'}
+
+with open('user_agents.txt') as f:
+    user_agents = f.read().splitlines()
+    f.close()
+
+shops = {'coolblue':{'urls':['https://www.coolblue.nl/product/865866/playstation-5.html', 
+                            'https://www.coolblue.nl/product/869587/sony-playstation-5-dualsense-draadloze-controller.html']
+                            }}
+
+users = {'Myrdin':'u2peec5j2cihqg6jp2ozez5v78nr8p',
+        'Sven':'us51jg8q6kw25hmugxgx8zp8r76nci'
+}
+
+def get_random_ua():
+    random_ua = ''
+    try:
+        prng = np.random.RandomState()
+        index = prng.permutation(len(user_agents) - 1)
+        idx = np.asarray(index, dtype=np.integer)[0]
+        random_proxy = user_agents[int(idx)]
+    except Exception as ex:
+        print('Exception in random_ua')
+        print(str(ex))
+    finally:
+        return random_ua
+
+def main_loop():
+    while True:
+        for func in shopfuncs:
+            func
+        time.sleep(100)
+
+def coolblue():
+    try:
+        print(get_random_ua())
+        req = requests.get(shops['coolblue']['urls'][1], headers={'User-Agent':get_random_ua()})
+        print("Coolblue - {} - {}".format(req.status_code, req.reason))
+        soup = BeautifulSoup(req.text, 'lxml')
+        in_cart = soup.find("button", {"class": "js-add-to-cart-button"})
+        if in_cart == None:
+            print("Out of stock")
+        elif in_cart != None:
+            print("In stock!!")
+            # send_notification('Coolblue', shops['coolblue']['urls'][0])
+    except:
+        print("Error for Coolblue, message: {} - {}".format(req.status_code, req.reason))
 
 
-urls = ['https://www.coolblue.nl/product/865866/playstation-5.html', 
-        'https://www.coolblue.nl/product/869587/sony-playstation-5-dualsense-draadloze-controller.html']
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'}
 
-while True:
-    soup = BeautifulSoup(requests.get(urls[0], headers=headers).text, 'lxml')
-    in_cart = soup.find("button", {"class": "js-add-to-cart-button"})
-    if in_cart == None:
-        print("Out of stock")
-    elif in_cart != None:
-        print("In stock!!")
-    time.sleep(100)
+
+
+
+def send_notification(shop, url):
+    for user in users:
+        print(user)
+        client = Client(users[user],
+                        api_token=Config.PUSHOVERTOKEN)
+        client.send_message('PS5 beschikbaar bij {}. Click link:'.format(shop), 
+                            title="PS5 in Stock!", url=url)
+
+
+
+shopfuncs = [coolblue()]
+main_loop()
+# send_notification('Coolblue', shops['coolblue']['urls'][0])
